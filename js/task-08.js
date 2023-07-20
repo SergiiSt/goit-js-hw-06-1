6595552a388b104814e5a985e63e6e3fbb859c43
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-07-21 00:45:38 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
-
+const loginEl = document.querySelector(".login-form");
+const emailEl = document.querySelector('[type = "email"]');
+const passwordEl = document.querySelector('[type="password"]');
+loginEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (emailEl.value === "" || passwordEl.value === "") {
+    return alert("Все поля должны быть заполнены!!!");
+  }
+  const dataArray = { email, password };
+  console.log(dataArray);
+  loginEl.reset();
+});
