f4b2bc149d900de958523318ef80086051ee2430
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-07-20 18:39:42 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listEl = document.querySelector("#ingredients");
+console.log(listEl);
+ingredients.forEach((ingridient) => {
+  const listElCall = document.createElement("li");
+  listElCall.textContent = ingridient;
+  listElCall.classList.add("item");
+  listEl.append(listElCall);
+});
