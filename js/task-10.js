b0a6da8cc7bafa3ed5e786f8dd49b941e1264b38
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-07-21 19:20:21 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+// Створи функцію createBoxes(amount), яка приймає один параметр - число.
+// Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX.
+// Використовуй готову функцію getRandomHexColor для отримання кольору.
+const inputEl = document.querySelector('[type="number"]');
+const createBtnEl = document.querySelector("[data-create]");
+const divContainerEl = document.querySelector("#boxes");
+const destroyBtnEl = document.querySelector("[data-destroy]");
+
+function createBoxes(value) {
+  let boxSize = 30;
+  for (let i = 0; i < value; i += 1) {
+    const createdDivEl = document.createElement("div");
+    createdDivEl.style.backgroundColor = getRandomHexColor();
+    createdDivEl.style.width = `${boxSize}px`;
+    createdDivEl.style.height = `${boxSize}px`;
+    divContainerEl.appendChild(createdDivEl);
+    boxSize += 10;
+  }
+}
+
+function destroyBoxes() {
+  divContainerEl.innerHTML = "";
+}
+
+createBtnEl.addEventListener("click", () => {
+  createBoxes(Number(inputEl.value));
+});
+
+destroyBtnEl.addEventListener("click", () => {
+  destroyBoxes();
+});
